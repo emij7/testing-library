7ad23dccb7b747c67a56001aab38483ac8ed1fe3
Ignacio Emiliano Juarez <emij_13@hotmail.com>
2023-03-02 18:54:05 -0300

    fix: better matching to get button element

--- src/components/register/register.test.jsx
+++ src/components/register/register.test.jsx
@@ -22,7 +22,9 @@ describe("Register component", () => {
   });
   it("should show error message when all the fields are not entered", async () => {
     render(<Register />);
-    const buttonElement = screen.getByRole("button");
+    const buttonElement = screen.getByRole("button", {
+      name: /register/i,
+    });
     await userEvent.click(buttonElement);
     const alertElement = screen.getByRole("alert");
     expect(alertElement).toBeInTheDocument();
